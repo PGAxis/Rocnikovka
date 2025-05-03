ce1bf4ca75fad3e625d273b9cea9d2c22035e8de
PG_Axis <axis.sixtikcz@gmail.com>
2025-05-03 11:25:14 +0200

    Add files via upload

---
+++ C#/Rocnikovka-Pi/Rocnikovka-Pi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocnikovka_Pi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 0.0;
+            double iterations = 1000000000;
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            for (double i = 0; i < iterations; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    pi += 1 / (2 * i + 1);
+                }
+                else
+                {
+                    pi -= 1 / (2 * i + 1);
+                }
+            }
+            pi = pi * 4;
+
+            sw.Stop();
+
+            Console.WriteLine($"Approximation of Pi: {pi}, ticks it took (ms): {sw.ElapsedMilliseconds}");
+            Console.ReadLine();
+        }
+    }
+}
