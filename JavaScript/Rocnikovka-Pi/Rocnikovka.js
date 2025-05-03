6007da66539e9406aa259d25e43ffd796e816b61
PG_Axis <axis.sixtikcz@gmail.com>
2025-05-03 11:31:42 +0200

    Add files via upload

---
+++ JavaScript/Rocnikovka-Pi/Rocnikovka.js
@@ -0,0 +1,18 @@
+function calculatePi(iterations) {
+    let pi = 0;
+    for (let i = 0; i < iterations; i++) {
+        if (i % 2 === 0) {
+            pi += 1 / (2 * i + 1);
+        } else {
+            pi -= 1 / (2 * i + 1);
+        }
+    }
+    return pi * 4;
+}
+
+const iterations = 1000000000;
+const start = performance.now();
+const pi = calculatePi(iterations);
+const end = performance.now();
+
+console.log("JavaScript speaking here. Approximation of Pi: " + pi + ", Execution time (ms): " + (end - start));
