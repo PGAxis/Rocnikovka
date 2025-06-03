457e6960e1fd92b1ecff5e483295cd7a74b9fc8d
PG_Axis <axis.sixtikcz@gmail.com>
2025-06-03 11:03:19 +0200

    Add files via upload

---
+++ Python/Benchmarks/matrix_multiplication_numpy.py
@@ -0,0 +1,20 @@
+import numpy as np
+import time
+
+def measure_time(fn):
+    start = time.time()
+    fn()
+    return time.time() - start
+
+def matrix_multiplication():
+    N = 300
+    A = np.random.rand(N, N)
+    B = np.random.rand(N, N)
+
+    def multiply():
+        C = np.dot(A, B)
+
+    duration = measure_time(multiply)
+    return duration
+
+print(f"Matrix multiplication using NumPy: {matrix_multiplication()} s")
